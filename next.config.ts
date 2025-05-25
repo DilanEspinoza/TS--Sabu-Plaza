81d8134fbe3ba6cf02e3dc6c9433b5ff53793940
Dilan Espinoza <dilan.developer22@gmail.com>
2025-05-25 13:50:00 -0500

    fix: change domain image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["i.imgur.com", "api.escuelajs.co", "'placeimg.com'"],
+		domains: ["fakestoreapi.com"],
 	},
 };
 
